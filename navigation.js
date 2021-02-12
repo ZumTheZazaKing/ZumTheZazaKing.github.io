1e92886422a5c4097917dd7fc830267268156e23
Purogurama Z <73376015+ZumTheZazaKing@users.noreply.github.com>
2021-02-13 00:09:52 +0800

    Add files via upload

---
+++ navigation.js
@@ -0,0 +1,28 @@
+//
+//
+//The following code is to make the navigation bar sticky
+//
+//
+const nav = document.getElementById('navigation');
+
+const topOfNav = nav.offsetTop;
+
+function fixNav(){
+
+    console.log(topOfNav, window.scrollY);
+
+    if(window.scrollY >= topOfNav){
+
+        document.body.style.paddingTop = nav.offsetHeight + 'px';
+        document.body.classList.add('fixed-nav');
+
+    } else {
+
+        document.body.style.paddingTop = 0;
+        document.body.classList.remove('fixed-nav');
+
+    }
+
+}
+
+window.addEventListener('scroll', fixNav);
