ab7a36959832c133b948f5dbcb49a0005d96cbd9
ZumTheZazaKing <zahidi85543@gmail.com>
2022-03-10 23:32:29 +0800

    Fixed warn message for mobile

--- src/components/MobileWarn.js
+++ src/components/MobileWarn.js
@@ -1,7 +1,7 @@
 export default function MobileWarn(){
     return (
         <div className="loading">
-            <h1>Please a larger screen<br/>(1200x600)</h1>
+            <h1>Please use a larger screen<br/>(1200x600)</h1>
         </div>
     )
 }
